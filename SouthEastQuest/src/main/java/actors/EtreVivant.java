d8f9cd5d396b130070c219b333dbd205a1e0e8a3
Zein1 <Zein1@users.noreply.github.com>
2019-07-10 11:22:29 +0200

    Update EtreVivant.java

---
+++ SouthEastQuest/src/main/java/actors/EtreVivant.java
@@ -0,0 +1,35 @@
+package actors;
+
+abstract class EtreVivant {
+	int level;
+	int hp;
+	int atk;
+	int def;
+	
+	
+	public EtreVivant(int level, int hp, int atk, int def) {
+		this.level = level;
+		this.hp = hp;
+		this.atk = atk;
+		this.def = def;
+	}
+
+	
+	/* Inflige 'degats' aux hp 
+	 * Renvoie TRUE si encore vivant, renvoie FALSE si mort
+	 */
+	public boolean calculDegats(int degats)
+	{
+		degats = degats - def;
+		hp = hp-degats;
+		if (hp < 0)
+		{
+			return false;
+		}
+		
+		return true;
+		
+		
+	}
+	
+}

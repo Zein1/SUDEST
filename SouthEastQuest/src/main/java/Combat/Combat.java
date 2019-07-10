77c323c8a6533876de75c80bb7e6ce08c39154d5
LucasJayet <lucas.jayet@campus-igs-toulouse.fr>
2019-07-10 15:31:40 +0200

    combat

--- SouthEastQuest/src/main/java/Combat/Combat.java
+++ SouthEastQuest/src/main/java/Combat/Combat.java
@@ -8,18 +8,18 @@ import java.util.Scanner;
 public class Combat {
     Monstre monstre;
     Humain  humain;
-    public static int nbrTour;
     public static Scanner sn = new Scanner(System.in);
 
     public Combat(Monstre monstre, Humain humain){
         this.humain = humain;
         this.monstre = monstre;
-        nbrTour = 0;
     }
 
-    public static void Fight(Humain humain, Monstre monstre){
+    public void Fight(Humain humain, Monstre monstre){
         while(humain.calculDegats(0)){
             while(monstre.calculDegats(0)){
+
+                int nbrTour = 0;
 
                 if(nbrTour %2 == 0) {
                     System.out.println("C'est votre tour ! Veullez choisir une attaque : \r\n");
@@ -38,12 +38,14 @@ public class Combat {
                         System.out.println("Veuillez choisir une attaque ");
                     }
                     System.out.println("C'est au tour de " + monstre.nom);
+                    nbrTour++;
                 }
                 else if(nbrTour %2 == 1)
                 {
                     monstre.calculDegats(monstre.atk);
                     System.out.println("Attention ! le monstre vous a infligé "+monstre.atk " de dégâts.");
                     System.out.println("Il vous reste " + humain.hp + " points de vie");
+                    nbrTour++;
                 }
             }
 
